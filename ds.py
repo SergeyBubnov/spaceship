ee56850c9f247edabaee325cda71d0d4afbb94cf
SergeyBubnov <i@welc.me>
2022-09-15 22:42:57 +0300

    pre-final

--- ds.py
+++ ds.py
@@ -70,8 +70,158 @@ class Perceptron(object):
         return np.where(self.net_input(X) >= 0.0, 1, -1)
 
 
-def plot_decision_regions(X, y, classifier, resolution=0.01):
+class AdalineGD(object):
+    """ADAptive LInear NEuron classifier.
 
+    Parameters
+    ------------
+    eta : float
+      Learning rate (between 0.0 and 1.0)
+    n_iter : int
+      Passes over the training dataset.
+    random_state : int
+      Random number generator seed for random weight
+      initialization.
+
+
+    Attributes
+    -----------
+    w_ : 1d-array
+      Weights after fitting.
+    cost_ : list
+      Sum-of-squares cost function value in each epoch.
+
+    """
+    def __init__(self, eta=0.01, n_iter=50, random_state=1):
+        self.eta = eta
+        self.n_iter = n_iter
+        self.random_state = random_state
+
+    def fit(self, X, y):
+        """ Fit training data.
+
+        Parameters
+        ----------
+        X : {array-like}, shape = [n_examples, n_features]
+          Training vectors, where n_examples is the number of examples and
+          n_features is the number of features.
+        y : array-like, shape = [n_examples]
+          Target values.
+
+        Returns
+        -------
+        self : object
+
+        """
+        rgen = np.random.RandomState(self.random_state)
+        self.w_ = rgen.normal(loc=0.0, scale=0.01, size=1 + X.shape[1])
+        self.cost_ = []
+        
+        for i in range(self.n_iter):
+            net_input = self.net_input(X)
+            # Please note that the "activation" method has no effect
+            # in the code since it is simply an identity function. We
+            # could write `output = self.net_input(X)` directly instead.
+            # The purpose of the activation is more conceptual, i.e.,  
+            # in the case of logistic regression (as we will see later), 
+            # we could change it to
+            # a sigmoid function to implement a logistic regression classifier.
+            output = self.activation(net_input)
+            errors = (y - output)
+            self.w_[1:] += self.eta * X.T.dot(errors)
+            self.w_[0] += self.eta * errors.sum()
+            cost = (errors**2).sum() / 2.0
+            self.cost_.append(cost)
+        return self
+
+    def net_input(self, X):
+        """Calculate net input"""
+        return np.dot(X, self.w_[1:]) + self.w_[0]
+
+    def activation(self, X):
+        """Compute linear activation"""
+        return X
+
+    def predict(self, X):
+        """Return class label after unit step"""
+        return np.where(self.activation(self.net_input(X)) >= 0.0, 1, -1)
+
+class LogisticRegressionGD(object):
+    """Logistic Regression Classifier using gradient descent.
+
+    Parameters
+    ------------
+    eta : float
+      Learning rate (between 0.0 and 1.0)
+    n_iter : int
+      Passes over the training dataset.
+    random_state : int
+      Random number generator seed for random weight
+      initialization.
+
+
+    Attributes
+    -----------
+    w_ : 1d-array
+      Weights after fitting.
+    cost_ : list
+      Logistic cost function value in each epoch.
+
+    """
+    def __init__(self, eta=0.05, n_iter=100, random_state=1):
+        self.eta = eta
+        self.n_iter = n_iter
+        self.random_state = random_state
+
+    def fit(self, X, y):
+        """ Fit training data.
+
+        Parameters
+        ----------
+        X : {array-like}, shape = [n_examples, n_features]
+          Training vectors, where n_examples is the number of examples and
+          n_features is the number of features.
+        y : array-like, shape = [n_examples]
+          Target values.
+
+        Returns
+        -------
+        self : object
+
+        """
+        rgen = np.random.RandomState(self.random_state)
+        self.w_ = rgen.normal(loc=0.0, scale=0.01, size=1 + X.shape[1])
+        self.cost_ = []
+
+        for i in range(self.n_iter):
+            net_input = self.net_input(X)
+            output = self.activation(net_input)
+            errors = (y - output)
+            self.w_[1:] += self.eta * X.T.dot(errors)
+            self.w_[0] += self.eta * errors.sum()
+            
+            # note that we compute the logistic `cost` now
+            # instead of the sum of squared errors cost
+            cost = -y.dot(np.log(output)) - ((1 - y).dot(np.log(1 - output)))
+            self.cost_.append(cost)
+        return self
+    
+    def net_input(self, X):
+        """Calculate net input"""
+        return np.dot(X, self.w_[1:]) + self.w_[0]
+
+    def activation(self, z):
+        """Compute logistic sigmoid activation"""
+        return 1. / (1. + np.exp(-np.clip(z, -250, 250)))
+
+    def predict(self, X):
+        """Return class label after unit step"""
+        return np.where(self.net_input(X) >= 0.0, 1, 0)
+        # equivalent to:
+        # return np.where(self.activation(self.net_input(X)) >= 0.5, 1, 0)
+
+def plot_decision_regions(X, y, classifier, resolution=0.01):
+    
     # setup marker generator and color map
     markers = ('s', 'x', 'o', '^', 'v')
     colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
@@ -100,7 +250,7 @@ def plot_decision_regions(X, y, classifier, resolution=0.01):
 
 
 #dataf should have column teacher_column, linear division
-def divide_frame(dataf,columns,teacher_column, random_state_sample = 1,random_state_ppn = 1,fraction = 0.7,eta = 0.1, iter = 50, plot = False,res = 0.1):
+def divide_frame(dataf,columns,teacher_column, classifier = 'Perceptron', random_state_sample = 1,random_state_ppn = 1,fraction = 0.7,eta = 0.1, iter = 50, plot = False,res = 0.1):
     dataf_extract = dataf.loc[:,columns+[teacher_column]]
     dataf_extract_train = dataf_extract.sample(random_state = random_state_sample, frac = fraction)
     dataf_extract_test = dataf_extract.drop(dataf_extract_train.index)
@@ -109,7 +259,14 @@ def divide_frame(dataf,columns,teacher_column, random_state_sample = 1,random_st
     X_test = dataf_extract_test.loc[:,columns].values
     y = dataf_extract_train.loc[:,teacher_column].values
     y_test = dataf_extract_test.loc[:,teacher_column].values
-    pnp = Perceptron(eta,iter,random_state_ppn)
+    if classifier == 'Perceptron':
+      pnp = Perceptron(eta,iter,random_state_ppn)
+    elif classifier == 'Adaline':
+      pnp = AdalineGD(eta,iter,random_state_ppn)
+    elif classifier == "LogisticRegression":
+      pnp = LogisticRegressionGD(eta,iter,random_state_ppn)
+    else:
+      print('Unexpected type of classifier')
 
     pnp.fit(X,y)
     if plot:
@@ -119,8 +276,29 @@ def divide_frame(dataf,columns,teacher_column, random_state_sample = 1,random_st
         plt.legend(loc='upper left')
 
     dataf_extract_test["prediction"] = pnp.predict(dataf_extract_test.loc[:,columns])
-    errors = abs(dataf_extract_test["prediction"]-dataf_extract_test[teacher_column])/2
+    errors = (dataf_extract_test["prediction"]-dataf_extract_test[teacher_column])/2
     
+    table = pd.DataFrame(index = [-1,1], columns = [-1,1])
+    
+    table.loc[-1,1] = 100*abs((errors<0).sum())/errors.count()
+    table.loc[1,-1] = 100*abs((errors>0).sum())/errors.count()
+    
+    arr = dataf_extract_test["prediction"]
+    
+    for i in arr.index:
+      if errors[i]!=0:
+        arr[i] = 0
+    
+    table.loc[-1,-1] = 100*abs((arr < 0).sum())/errors.count()
+    table.loc[1,1] = 100*abs((arr > 0).sum())/errors.count()
+    
+    errors = abs(errors)
     percent = 100*errors.sum()/errors.count()
-    print(errors.count(), f" total in test, errors: {percent:.2f}%")
+    print("test size: ", errors.count(), f", total errors in test: {percent:.2f}%")
+    print("index = predicted, columns = factual, in %:")
+    
+    print(table)
+    
+    #percent = 100*errors.sum()/errors.count()
+    #print(errors.count(), f" total in test, errors: {percent:.2f}%")
     return pnp
